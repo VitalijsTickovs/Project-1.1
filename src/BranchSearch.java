f0930ad59999eb6e87fcf94f960cb338be356f0e
VitalijsTickovs <v.tickovs@student.maastrichtuniversity.nl>
2021-10-07 12:30:26 +0200

    BranchSearch with comments

--- src/BranchSearch.java
+++ src/BranchSearch.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
 public class BranchSearch {
     public static final int horizontalGridSize = 5;
-    public static final int verticalGridSize = 8;
+    public static final int verticalGridSize = 10;
     
-    public static final char[] input = { 'F','Y','T','N','L','P','I','V'};
+    public static final char[] input = { 'N','U','T','F','L','P','I','Y','Z','X', 'W', 'V'};
     
     
     //Static UI class to display the board
@@ -15,42 +15,53 @@ public class BranchSearch {
     public static void search(){
         // Initialize an empty board
         int[][] field = new int[horizontalGridSize][verticalGridSize];
-		initTable(field,'i');
+		removeLetter(field,'i');
+
+		//This is branching algorithm
 		if(!searchBranching(field, input, 0)){
-			System.out.println("The solution is not found");
-		}
+			System.out.println("The solution is not found");	//In case searchBranching is false, it will output that there is no solution
+		}														//If it was in base case, it would output this text for every branch, where there is no soulution
     }
-
+	/**
+	 * This function converts the letters from the input to the ID's for 
+	 * @param character
+	 * @return shape ID
+	 */
     public static int characterToID(char character) {
-    	int pentID = -1; 
+    	int shapeID = -1; 
     	if (character == 'X') {
-    		pentID = 0;
+    		shapeID = 0;
     	} else if (character == 'I') {
-    		pentID = 1;
+    		shapeID = 1;
     	} else if (character == 'Z') {
-    		pentID = 2;
+    		shapeID = 2;
     	} else if (character == 'T') {
-    		pentID = 3;
+    		shapeID = 3;
     	} else if (character == 'U') {
-    		pentID = 4;
+    		shapeID = 4;
      	} else if (character == 'V') {
-     		pentID = 5;
+			shapeID = 5;
      	} else if (character == 'W') {
-     		pentID = 6;
+			shapeID = 6;
      	} else if (character == 'Y') {
-     		pentID = 7;
+			shapeID = 7;
     	} else if (character == 'L') {
-    		pentID = 8;
+    		shapeID = 8;
     	} else if (character == 'P') {
-    		pentID = 9;
+    		shapeID = 9;
     	} else if (character == 'N') {
-    		pentID = 10;
+    		shapeID = 10;
     	} else if (character == 'F') {
-    		pentID = 11;
+    		shapeID = 11;
     	} 
-    	return pentID;
+    	return shapeID;
     }
 
+	/**
+	 * This method checks if the field matrix is full
+	 * @param field
+	 * @return true, if the matrix is full || false, if the matrix still has a spot to fill in 
+	 */
     public static boolean isFull(int[][] field){
 		boolean isFull = true;
 		for(int i=0;i<field.length;i++){
@@ -61,6 +72,14 @@ public class BranchSearch {
 		return isFull;
 	}
 
+	/**
+	 * this method adds the shape to the field matrix, changing the values of it
+	 * @param field - the matrix to which we have to add the shape
+	 * @param piece - the shape matrix
+	 * @param pieceID - the ID of the letter
+	 * @param x - the horixontal position where to place the shape in field matrix
+	 * @param y - the vertical position where to place the shape in field matrix
+	 */
     public static void addPiece(int[][] field, int[][] piece, int pieceID, int x, int y)
     {
         for(int i = 0; i < piece.length; i++) // loop over x position of pentomino
@@ -76,16 +95,14 @@ public class BranchSearch {
         }
     }
 
-    public static void printTable(int[][] field){
-        for(int i=0; i<field.length;i++){
-            for(int j=0;j<field[0].length;j++){
-                System.out.print(field[i][j]);
-            }
-            System.out.println();
-        }
-    }
-
-	public static int[][] initTable(int[][] field, char user){
+	/**
+	 * This removes the current letter of the branch, to try another position, if possible
+	 * @code user == 'i' is to initialize the table at the start
+	 * @param field
+	 * @param user
+	 * @return field, which is removed from 
+	 */
+	public static int[][] removeLetter(int[][] field, char user){
 		for(int i = 0; i < field.length; i++)
         {
             for(int j = 0; j < field[i].length; j++)
@@ -98,17 +115,23 @@ public class BranchSearch {
 		return field;
 	}
 
+	/**
+	 * This is the branching method, which is recursive, non-deterministic, depth-first, backtracking algorithm
+	 * @param field is matrix to fill in with pentominoes
+	 * @param userInput letters that are inputed by the user
+	 * @param mutation keeps track on which mutation we are
+	 * @return if true returns visual representation of the filled table || returns a text where that there is no solution
+	 */
     public static boolean searchBranching(int[][] field, char[] userInput, int mutation){
 		if(isFull(field)){									//base case 1, where the shapes are in our matrix
 			ui.setState(field);
-			printTable(field);
 			System.out.println("The solution is found");
 			return true;
 		}else if(userInput.length ==0){						//base case 2, where the there is no characters left
 			return false;
 		}else{
-			//make a copy of existing matrix
-			int[][] newField = Arrays.copyOfRange(field, 0, field.length);							
+			//making a copy of existing matrix
+			int[][] newField = Arrays.copyOfRange(field, 0, field.length);						
 			//find userInput[0] char from database
 			int pieceID = characterToID(userInput[0]);
 			int[][] pieceToPlace = PentominoDatabase.data[pieceID][mutation];
@@ -124,32 +147,32 @@ public class BranchSearch {
 							}
 						}
 					}
+					//if the shape can be placed in the matrix, it adds it and tries to fit next shape
 					if(placeFound){
 						addPiece(newField, pieceToPlace, pieceID, i,j);
 						if(searchBranching(newField, Arrays.copyOfRange(userInput, 1, userInput.length), 0)){
 							return true;
 						}
 					}
-					//newField = Arrays.copyOfRange(field, 0, field.length);
-					newField = initTable(field, userInput[0]);
-					ui.setState(field);
+					//if it is returned false, then remove that shape, to try another position
+					newField = removeLetter(field, userInput[0]);
 				}
 			}
 			//try the different mutation for the shape
 			if((mutation<7) && (mutation < PentominoDatabase.data[pieceID].length-1)){
 				return searchBranching(field, userInput, mutation+1);
 			}else{
-				//return searchBranching(initTable(field), Arrays.copyOfRange(userInput, 1, userInput.length), 0);
-				return false;
+				return false;	//if the mutations are finished it will return false
 			}
 		}
 	}
-    public static void main(String[] args)
+	//The main method is needed to run the algorithm
+	public static void main(String[] args)
     {
-        long startTime = System.currentTimeMillis();
+		long startTime = System.currentTimeMillis();
         search();
-		final long endTime = System.currentTimeMillis();
+		long endTime = System.currentTimeMillis();
 
-		System.out.println("Total execution time: " + (endTime - startTime));
+		System.out.println("Total execution time: " + (endTime - startTime) + " nanoseconds");
     }
 }
