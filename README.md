424d96a2c3813a3e2749fca554d6ec7bcb4f539b
Oscarvanb <oscarvanbommel@gmail.com>
2021-10-07 18:27:20 +0200

    final changes

--- README.md
+++
@@ -1,5 +0,0 @@
-# Project-1.1
-test
-test
-test
-another test
--- src/Dancing_Run.java
+++ src/Dancing_Run.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 
-public class test {
+public class Dancing_Run {
 
 
-    public static final int horizontalGridSize = 5;
+    public static final int horizontalGridSize = 6;
 
     //for efficiency make vertical grid size the large one:)
-    public static final int verticalGridSize = 13;
+    public static final int verticalGridSize = 10;
 
     public static UI ui = new UI(verticalGridSize,horizontalGridSize,50);
     
-    public static final char[] input = { 'L','F','I','X','U','V','N','T','W','P','Z','Y','I'};
+    public static final char[] input = { 'L','F','X','U','V','N','T','W','P','Z','Y','I'};
     //public static final char[] input = { 'I','X','L','Z','F','P','W','N','V'};
     //public static final char[] input = {'L','V','N'};
    public static void main(String[] args) {
